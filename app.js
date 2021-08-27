5a33a9850b8a58c626b878bbe93afa8b48c78d51
Raj <rajraghuwanshi393@gmail.com>
2021-08-27 00:16:02 -0500

    oauth added

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const ejs = require('ejs');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 app.set("view engine", "ejs");
@@ -26,18 +28,50 @@ mongoose.connect("mongodb://localhost:27017/secretsdb", {useNewUrlParser: true, 
 
 const secretSchema = new mongoose.Schema({
   username:String,
-  password:String
+  password:String,
+  googleId:String
 });
 
 secretSchema.plugin(passportLocalMongoose);
+secretSchema.plugin(findOrCreate);
 
 const Secret = mongoose.model("Secret", secretSchema);
 
 
 passport.use(Secret.createStrategy());
 
-passport.serializeUser(Secret.serializeUser());
-passport.deserializeUser(Secret.deserializeUser());
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  Secret.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secret"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    Secret.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] })
+);
+
+app.get('/auth/google/secret',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secret');
+  });
 
 app.get("/", function (req , res){
   res.render("home");
